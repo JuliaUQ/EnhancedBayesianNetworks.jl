5c31a4a1e20862199c392d529fdb650009ea1cb2
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-02-06 13:44:50 +0100

    Add test for base.isempty for Discretization structs

--- test/nodes/nodes.jl
+++ test/nodes/nodes.jl
@@ -5,6 +5,8 @@
         interval = [-1, 0, 1, 3]
         exact_interval = ExactDiscretization([-1, 0, 1, 3])
         @test exact_interval.intervals == interval
+        @test !isempty(exact_interval)
+        @test isempty(ExactDiscretization())
     end
     @testset "Approximated Discretization" begin
         interval = [-1, 0, 3, 1]
@@ -18,5 +20,7 @@
         sigma = 2
         approx_interval = ApproximatedDiscretization([-1, 0, 1, 3], 2)
         @test approx_interval.intervals == interval
+        @test !isempty(approx_interval)
+        @test isempty(ApproximatedDiscretization())
     end
 end
