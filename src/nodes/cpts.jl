7bd96a4b4da5a40276ade5f58daa58562bfbf45b
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-02-19 14:11:18 +0100

    Add a setindex function for continuous cpt to avoid probabilities check

--- src/nodes/cpts.jl
+++ src/nodes/cpts.jl
@@ -12,8 +12,26 @@ struct ConditionalProbabilityTable{T<:Union{ContinuousProbability,DiscreteProbab
     end
 end
 
-function Base.setindex!(cpt::ConditionalProbabilityTable, value, key...)
+function Base.setindex!(cpt::ConditionalProbabilityTable{DiscreteProbability}, value, key...)
     verify_probability_value(value)
+    selector = map((p) -> p[1] => ByRow(x -> x == p[2]), collect(key))
+    evidence_nodes = collect(map(p -> p[1], key))
+    cpt_nodes = Symbol.(filter(i -> i != "Π", names(cpt.data)))
+    if issetequal(evidence_nodes, cpt_nodes)
+        cp = subset(cpt.data, selector, view=true)
+        if isempty(cp)
+            push!(cpt.data, (key..., Π=value))
+        else
+            @assert size(cp, 1) == 1
+            cp.Π[1] = value
+        end
+    else
+        error("Cannot set index with $evidence_nodes into a CPT initialized with $cpt_nodes")
+    end
+    return nothing
+end
+
+function Base.setindex!(cpt::ConditionalProbabilityTable{ContinuousProbability}, value, key...)
     selector = map((p) -> p[1] => ByRow(x -> x == p[2]), collect(key))
     evidence_nodes = collect(map(p -> p[1], key))
     cpt_nodes = Symbol.(filter(i -> i != "Π", names(cpt.data)))
