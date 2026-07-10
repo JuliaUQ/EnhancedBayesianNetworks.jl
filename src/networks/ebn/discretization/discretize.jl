c03a6c111f56bf8541864f7eddc8f5d3c90165ce
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-07-10 17:29:37 +0200

    Remove min/max function from scalar operations

--- src/networks/ebn/discretization/discretize.jl
+++ src/networks/ebn/discretization/discretize.jl
@@ -20,20 +20,20 @@ function _format_interval(node::ContinuousNode)
     min = node.discretization.intervals[1]
     max = node.discretization.intervals[end]
     lower_bound, upper_bound = _distribution_bounds(node)
-    if minimum(min) > lower_bound
+    if min > lower_bound
         @warn "node $(repr(node.name)) has minimum intervals value $min > support lower bound $lower_bound. Lower bound will be used as intervals start"
         insert!(intervals, 1, lower_bound)
     end
-    if minimum(min) < lower_bound
+    if min < lower_bound
         @warn "node $(repr(node.name)) has minimum intervals value $min < support lower bound $lower_bound. Lower bound will be used as intervals start"
         deleteat!(intervals, intervals .<= lower_bound)
         insert!(intervals, 1, lower_bound)
     end
-    if maximum(max) < upper_bound
+    if max < upper_bound
         @warn "node $(repr(node.name)) has maximum intervals value $max < support upper bound $upper_bound. Upper bound will be used as intervals end"
         push!(intervals, upper_bound)
     end
-    if maximum(max) > upper_bound
+    if max > upper_bound
         @warn "node $(repr(node.name)) has maximum intervals value $max > support upper bound $upper_bound. Upper bound will be used as intervals end"
         deleteat!(intervals, intervals .>= upper_bound)
         push!(intervals, upper_bound)
