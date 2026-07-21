a65f02cb43e3d1f97884ac1a0403ad3ec10000fe
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-07-21 15:29:01 +0200

    Setup DocumenterVitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,23 @@
 using Documenter
 using DocumenterCitations
+using DocumenterVitepress
 using EnhancedBayesianNetworks
 
 bib=CitationBibliography(joinpath(@__DIR__, "citations.bib"))
 
-makedocs(; modules=[EnhancedBayesianNetworks], plugins=[bib], authors="Andrea Perin & Jasper Behrensdorf", pages=["Home" => "index.md", "References" => "references.md"], sitename="EnhancedBayesianNetworks.jl", source="src", build="build", warnonly=true, format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"), draft=false)
+format = DocumenterVitepress.MarkdownVitepress(
+    repo="https://github.com/JuliaUQ/EnhancedBayesianNetworks.jl",
+    devbranch="main",
+    devurl="dev")
+
+pages=["Home" => "index.md", "References" => "references.md"]
+
+makedocs(; modules=[EnhancedBayesianNetworks], plugins=[bib], authors="Andrea Perin & Jasper Behrensdorf", pages=pages, sitename="EnhancedBayesianNetworks.jl", source="src", build="build", warnonly=true, format=format, draft=false)
+
+DocumenterVitepress.deploydocs(;
+    repo="github.com/JuliaUQ/EnhancedBayesianNetworks.jl",
+    target=joinpath(@__DIR__, "build"),
+    branch="gh-pages",
+    devbranch="main",
+    push_preview=true,
+)
