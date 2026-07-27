1b35f1d30dbed4dcb0008b268aaa451de2fc728a
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-07-27 16:00:42 +0200

    Refactor utils/verify

--- src/networks/ebn/ebn.jl
+++ src/networks/ebn/ebn.jl
@@ -1,6 +1,3 @@
-
-include("../../utils/verify/cyclicality_and_connection.jl")
-include("../../utils/verify/add_child.jl")
 include("discretization/discretize.jl")
 include("enhancedbn.jl")
 include("reduction/evaluate_node.jl")
