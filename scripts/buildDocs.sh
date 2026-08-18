9074b7c96351bd0300c3001cafa8d0ca1e37b0d1
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-08-18 13:26:52 +0200

    Add buildDocs.sh script

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -2,6 +2,9 @@
 
 export GKSwstype=100
 
+
 julia --project=docs/ -e 'using Pkg; Pkg.develop(path=pwd()); Pkg.update()'
+
+julia --project=docs/ docs/literateDocs.jl
 
 julia --project=docs/ docs/make.jl
