dcdc915ae13fdbc8d205fae6f8f4a9d65fb432d2
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-08-20 14:28:51 +0200

    Add vehicle example

--- docs/make.jl
+++ docs/make.jl
@@ -45,6 +45,7 @@ pages = [
         ],
         "Enhanced Bayesian Networks" => [
             "One-Bay Frame (discretization)" => "examples/EnhancedBayesianNetworks/one-bay_elastoplastic_frame.md",
+            "Vehicle Suspension" => "examples/EnhancedBayesianNetworks/vehicle-3D-suspension.md",
         ],
     ],
     "API" => [
