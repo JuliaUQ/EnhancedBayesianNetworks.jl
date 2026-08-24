ef9e8055ee77fa1278fa4a8bc8a2be4167fbd500
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-08-24 12:22:28 +0200

    Add ProgressMeter

--- src/EnhancedBayesianNetworks.jl
+++ src/EnhancedBayesianNetworks.jl
@@ -1,12 +1,13 @@
 module EnhancedBayesianNetworks
 
+using Compose
 using DataFrames
 using Distributions
 using LinearAlgebra
 using Polyhedra: HalfSpace, doubledescription
+using ProgressMeter: Progress, next!
 using Reexport
 using SparseArrays
-using Compose
 
 @reexport using UncertaintyQuantification
 
